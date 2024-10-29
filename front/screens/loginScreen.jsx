428cacc2706a9ab98126d1d9546a1129122c7355
Jules Sorensen <julessorens@gmail.com>
2024-10-29 15:06:50 +0100

    feat(auth): use react native paper
    React Native Paper package + login and register refacto

--- front/screens/loginScreen.jsx
+++ front/screens/loginScreen.jsx
@@ -1,58 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { Alert, View, Text, TextInput, Button, Pressable, StyleSheet } from 'react-native';
-import { loginInApp, formatErrorMessage } from '../services/login';
-import { checkAccount } from '../services/checkAccount';
+import React, { useState, useEffect } from "react";
+import { Alert, View, Text, Pressable, StyleSheet } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { loginInApp, formatErrorMessage } from "../services/login";
+import { checkAccount } from "../services/checkAccount";
 
 const LoginScreen = ({ navigation }) => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const connect = async () => {
-
+    setIsLoaded(true);
     try {
-      await loginInApp(email, password, navigation)
+      await loginInApp(email, password, navigation);
     } catch (error) {
-
       Alert.alert(
-        'Une erreur est survenue',
+        "Une erreur est survenue",
         formatErrorMessage(error),
         [
           {
-            text: 'Compris',
+            text: "Compris",
           },
         ],
         {
           cancelable: true,
         },
       );
+    } finally {
+      setIsLoaded(false);
     }
-  }
-  
-  useEffect(()=> {
-    checkAccount(navigation)
-  }, [])
+  };
+
+  useEffect(() => {
+    checkAccount(navigation);
+  }, []);
 
   return (
     <View style={styles.container}>
-
       <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email :</Text>
-        <TextInput style={styles.input} placeholder='Entrez votre email' onChangeText={setEmail} value={email} />
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          label="Email"
+          mode="outlined"
+        />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.label}>Mot de passe :</Text>
-        <TextInput style={styles.input} placeholder='Entrez votre mot de passe' onChangeText={setPassword} value={password} secureTextEntry />
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          label="Mot de passe"
+          mode="outlined"
+          secureTextEntry
+        />
       </View>
 
       <View style={styles.buttonContainer}>
-        <Button title="Allons y" onPress={connect} />
-        <Pressable onPress={() => navigation.navigate('Register')}>
-          <Text>JE VEUX M'INSCRIRE</Text>
+        <Button
+          children="Allons y"
+          mode="elevated"
+          onPress={connect}
+          loading={isLoaded}
+          disabled={isLoaded}
+        />
+        <Pressable onPress={() => navigation.navigate("Register")}>
+          <Text style={styles.redirectionText}>JE VEUX M'INSCRIRE</Text>
         </Pressable>
       </View>
-
     </View>
   );
 };
@@ -61,27 +78,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 20,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: "space-between"
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   label: {
     width: 120,
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   input: {
     flex: 1,
     height: 40,
     paddingHorizontal: 10,
+  },
+  redirectionText: {
+    opacity: 0.7,
+    color: "blue",
+    marginTop: 20,
+    textDecorationLine: "underline",
   },
 });
 
--- front/screens/registerScreen.jsx
+++ front/screens/registerScreen.jsx
@@ -1,63 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { Alert,View, Text, Button, TextInput, StyleSheet, Pressable } from 'react-native';
-import { register } from '../services/register';
-import { loginInApp, formatErrorMessage  } from '../services/login';
-import { checkAccount } from '../services/checkAccount';
+import React, { useState, useEffect } from "react";
+import { Alert, View, Text, StyleSheet, Pressable } from "react-native";
+import { register } from "../services/register";
+import { loginInApp, formatErrorMessage } from "../services/login";
+import { checkAccount } from "../services/checkAccount";
+import { Button, TextInput } from "react-native-paper";
 
 const RegisterScreen = ({ navigation }) => {
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const createUserInBDDViaAPIURL = async () => {
+    setIsLoaded(true);
+
     try {
-      await register({ email: email, name: name, password: password })
-      await loginInApp(email, password, navigation)
+      await register({ email: email, name: name, password: password });
+      await loginInApp(email, password, navigation);
     } catch (error) {
       Alert.alert(
-        'Une erreur est survenue',
+        "Une erreur est survenue",
         formatErrorMessage(error),
         [
           {
-            text: 'Compris',
+            text: "Compris",
           },
         ],
         {
           cancelable: true,
         },
       );
+    } finally {
+      setIsLoaded(false);
     }
-  }
-  
-  useEffect(()=> {
-    checkAccount(navigation)
-  }, [])
+  };
+
+  useEffect(() => {
+    checkAccount(navigation);
+  }, []);
 
   return (
     <View style={styles.container}>
-
       <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email :</Text>
-        <TextInput style={styles.input} placeholder='Entrez votre email' onChangeText={setEmail} value={email} />
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          label="Email"
+          mode="outlined"
+        />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.label}>Nom utilisateur :</Text>
-        <TextInput style={styles.input} placeholder='Entrez votre nom utilisateur' onChangeText={setName} value={name} />
+        <TextInput
+          style={styles.input}
+          placeholder="Entrez votre nom utilisateur"
+          onChangeText={setName}
+          value={name}
+          label="Nom"
+          mode="outlined"
+        />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.label}>Mot de passe :</Text>
-        <TextInput style={styles.input} placeholder='Entrez votre mot de passe' onChangeText={setPassword} value={password} secureTextEntry />
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          label="Mot de passe"
+          mode="outlined"
+          secureTextEntry
+        />
       </View>
 
       <View style={styles.buttonContainer}>
-        <Button title="Allons y" onPress={createUserInBDDViaAPIURL} />
-        <Pressable onPress={() => navigation.navigate('Login')}>
-          <Text> J'ai déjà un compte</Text>
+        <Button
+          children="Allons y"
+          mode="elevated"
+          onPress={createUserInBDDViaAPIURL}
+          loading={isLoaded}
+          disabled={isLoaded}
+        />
+        <Pressable onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.redirectionText}>(J'ai déjà un compte)</Text>
         </Pressable>
       </View>
-
     </View>
   );
 };
@@ -66,29 +93,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 20,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: "space-between"
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   label: {
     width: 120,
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   input: {
     flex: 1,
     height: 40,
     paddingHorizontal: 10,
   },
+  redirectionText: {
+    opacity: 0.7,
+    color: "blue",
+    marginTop: 20,
+    fontStyle: "italic",
+    textDecorationLine: "underline",
+  },
 });
-
 
 export default RegisterScreen;
